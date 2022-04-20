bbc96c07629051cd27564034d4750675eecb060a
Ingrzs <ingramirozs@gmail.com>
2022-04-20 14:11:21 -0500

    añadiendo get a la clase

--- spideverse/spiderverse/app/spiderman.js
+++ spideverse/spiderverse/app/spiderman.js
@@ -6,6 +6,11 @@ this.actor = actor
 this.npeliculas = npeliculas
 this.cine = cine
 }
+
+getinfo(){
+    return `hola soy ${this.name} de ${this.cine}`
+}
+
 }
 //aqui exportamos este clase a test pero puedo usarsen en otro lugar ya que tiene export
 module.exports = spiderman
