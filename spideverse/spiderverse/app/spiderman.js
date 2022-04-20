280810399c8e75d6698026a4b050cd948ebc767c
Ingrzs <ingramirozs@gmail.com>
2022-04-20 14:00:17 -0500

    clase añadida

--- spideverse/spiderverse/app/spiderman.js
+++ spideverse/spiderverse/app/spiderman.js
@@ -1,7 +1,11 @@
-class spiderman (name, edad, actor,npeliculas,cine){
+class spiderman { constructor(name, edad, actor,npeliculas,cine)
+{
 this.name = name
 this.edad = edad
 this.actor = actor
 this.npeliculas = npeliculas
 this.cine = cine
 }
+}
+//aqui exportamos este clase a test pero puedo usarsen en otro lugar ya que tiene export
+module.exports = spiderman
